634899f8157301fe5e7f7b95c6aeba1d0e9560ca
Alan-r15 <52107793+Alan-r15@users.noreply.github.com>
2019-06-22 14:12:04 -0600

    Add files via upload

---
+++ mlmodel.py
@@ -0,0 +1,28 @@
+from tensorflow import keras
+from PIL import Image
+import numpy as np
+
+class mlmodel():
+	def __init__(self):
+		self.team = "Los voladores de Papantla"
+		self.name = "Hentai Lover De Durango"
+		self.model = keras.models.load_model("Aver(1).model",compile=False)
+		self.labels = ["Apple","Banana","Orange","Mixed"]
+
+	def predict(self,path = "Appleasa.jpg"):
+		top,right,bottom,left = (0,0,0,0)
+		coords = (top,right,bottom,left)
+
+		try:
+			im = Image.open(path).convert('L')
+			im = im.resize((64,64))
+			np_im = np.array(im)  # convert to array
+			np_im = np_im.reshape(-1,64,64,1)
+
+			prediction = self.model.predict(np_im)
+			predicted_fruits  = []
+			for i in range(0,4):
+				predicted_fruits.append((self.labels[i],prediction[0,i],coords))
+			return predicted_fruits
+		except:
+			return None
